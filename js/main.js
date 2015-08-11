16e106f731f8a905790d5b0d8a99692f0136f87a
sole <sole@mozilla.com>
2015-08-11 18:43:11 +0100

    workaround for contentWindow being null

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 window.onload = function() {
+
 	var h1 = document.querySelector('h1');
 	h1.textContent = document.title;
 	console.log('hey');
 
+	var iframes = [];
 	var num = 3;
 
 	for(var i = 0; i < num; i++) {
 		var fr = document.createElement('iframe');
 		var src = 'iframe.html#' + i;
 		fr.src = src;
+		
 		console.log(src);
 		document.body.appendChild(fr);
 
-		fr.contentWindow.addEventListener('load', function() {
-			fr.contentWindow.postMessage('hey', '*');
-		});
+		iframes.push(fr);
+
+		var frameid = i;
+
+		// workaround for https://github.com/servo/servo/issues/7162
+		fr.onload = function() {
+			//this.window.postMessage('hey', '*');
+			this.contentWindow.start('aaa');
+		};
 	}
+
+	console.log('pfff', iframes);
 };
